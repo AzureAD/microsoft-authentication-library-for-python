eb38e8bc678e83003b1192a1384162d4c13673ac
Ray Luo <rayluo@microsoft.com>
2016-09-08 14:18:14 -0700

    Implement an AuthenticationResult equivalent

--- msal/request.py
+++ msal/request.py
@@ -1,3 +1,5 @@
+import time
+
 from . import oauth2
 from .exceptions import MsalServiceError
 
@@ -14,19 +16,41 @@ class BaseRequest(object):
         self.__dict__.update(locals())
 
     def run(self):
+        """Returns a dictionary, which typically contains following keys:
+
+        * token: A string containing an access token (or id token)
+        * expires_on: A timestamp, in seconds. So compare it with time.time().
+        * user: TBD
+        * and some other keys from the wire, such as "scope", "id_token", etc.,
+          which may or may not appear in every different grant flow.
+          So you should NOT assume their existence,
+          instead you would need to access them safely by dict.get('...').
+        """
         # TODO Some cache stuff here
         raw = self.get_token()
         if 'error' in raw:
             raise MsalServiceError(**raw)
         # TODO: Deal with refresh_token
-        return {  # i.e. the AuthenticationResult
-            "token": raw.get('access_token'),
-            "expires_on": raw.get('expires_in'),  # TODO: Change into EPOCH
-            "tenant_id": None,  # TODO
-            "user": None,  # TODO
-            "id_token": None,  # TODO
-            "scope": set([]),  # TODO
+
+        # Keep (most) contents in raw token response, extend it, and return it
+        raw['token'] = raw.get('access_token') or raw.get('id_token')
+        raw['expires_on'] = self.__timestamp(
+            # A timestamp is chosen because it is more lighweight than Datetime,
+            # and then the entire return value can be serialized as JSON string,
+            # should the developers choose to do so.
+            # This is the same timestamp format used in JWT's "iat", by the way.
+            raw.get('expires_in') or raw.get('id_token_expires_in'))
+        if 'scope' in raw:
+            raw['scope'] = set(raw['scope'].split())  # Using SPACE as delimiter
+        raw['user'] = {  # Contents derived from raw['id_token']
+            # TODO: Follow https://github.com/AzureAD/microsoft-authentication-library-for-android/blob/dev/msal/src/internal/java/com/microsoft/identity/client/IdToken.java
+            # https://github.com/AzureAD/microsoft-authentication-library-for-android/blob/dev/msal/src/internal/java/com/microsoft/identity/client/User.java
             }
+        return raw  # equivalent to AuthenticationResult in other MSAL SDKs
+
+    def __timestamp(self, seconds_from_now=None):  # Returns timestamp IN SECOND
+        return time.time() + (
+            seconds_from_now if seconds_from_now is not None else 3600)
 
     def get_token(self):
         raise NotImplemented("Use proper sub-class instead")
