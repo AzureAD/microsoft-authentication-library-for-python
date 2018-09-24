d93a05d15f961fc02b2b05be1aca1313fe93697a
Ray Luo <rayluo.mba@gmail.com>
2018-09-24 01:28:51 -0700

    Test case for auth code grant

--- msal/application.py
+++ msal/application.py
@@ -1,16 +1,44 @@
 from oauth2cli import Client
 from .authority import Authority
-from .request import decorate_scope
 from .assertion import create_jwt_assertion
+
+
+def decorate_scope(
+        scope, client_id,
+        policy=None,  # obsolete
+        reserved_scope=frozenset(['openid', 'profile', 'offline_access'])):
+    scope_set = set(scope)  # Input scope is typically a list. Copy it to a set.
+    if scope_set & reserved_scope:
+        # These scopes are reserved for the API to provide good experience.
+        # We could make the developer pass these and then if they do they will
+        # come back asking why they don't see refresh token or user information.
+        raise ValueError(
+            "API does not accept {} value as user-provided scopes".format(
+                reserved_scope))
+    if client_id in scope_set:
+        if len(scope_set) > 1:
+            # We make developers pass their client id, so that they can express
+            # the intent that they want the token for themselves (their own
+            # app).
+            # If we do not restrict them to passing only client id then they
+            # could write code where they expect an id token but end up getting
+            # access_token.
+            raise ValueError("Client Id can only be provided as a single scope")
+        decorated = set(reserved_scope)  # Make a writable copy
+    else:
+        decorated = scope_set | reserved_scope
+    return decorated
 
 
 class ClientApplication(object):
 
     def __init__(
             self, client_id,
+            client_credential=None,
             authority="https://login.microsoftonline.com/common/",
             validate_authority=True):
         self.client_id = client_id
+        self.client_credential = client_credential
         self.authority = Authority(authority, validate_authority)
             # Here the self.authority is not the same type as authority in input
 
@@ -26,6 +54,81 @@ class ClientApplication(object):
                 'client_assertion_type': type_, 'client_assertion': assertion}
         else:
             return {'client_secret': client_credential}
+
+    def get_authorization_request_url(
+            self,
+            scope,
+            additional_scope=frozenset([]),  # Not yet supported
+            login_hint=None,
+            state=None,  # Recommended by OAuth2 for CSRF protection
+            redirect_uri=None,
+            authority=None,  # By default, it will use self.authority;
+                             # Multi-tenant app can use new authority on demand
+            response_type="code",  # Can be "token" if you use Implicit Grant
+            **kwargs):
+        """Constructs a URL for you to start a Authorization Code Grant.
+
+        :param scope: Scope refers to the resource that will be used in the
+            resulting token's audience.
+        :param additional_scope: Additional scope is a concept only in AAD.
+            It refers to other resources you might want to prompt to consent
+            for in the same interaction, but for which you won't get back a
+            token for in this particular operation.
+            (Under the hood, we simply merge scope and additional_scope before
+            sending them on the wire.)
+        :param str state: Recommended by OAuth2 for CSRF protection.
+        """
+        the_authority = Authority(authority) if authority else self.authority
+        client = Client(
+            self.client_id,
+            authorization_endpoint=the_authority.authorization_endpoint)
+        return client.build_auth_request_uri(
+            response_type="code",  # Using Authorization Code grant
+            redirect_uri=redirect_uri, state=state, login_hint=login_hint,
+            scope=decorate_scope(scope, self.client_id),
+            )
+
+    def acquire_token_with_authorization_code(
+            self,
+            code,
+            scope,  # Syntactically required. STS accepts empty value though.
+            redirect_uri=None,
+                # REQUIRED, if the "redirect_uri" parameter was included in the
+                # authorization request as described in Section 4.1.1, and their
+                # values MUST be identical.
+            ):
+        """The second half of the Authorization Code Grant.
+
+        :param code: The authorization code returned from Authorization Server.
+        :param scope:
+
+            If you requested user consent for multiple resources, here you will
+            typically want to provide a subset of what you required in AuthCode.
+
+            OAuth2 was designed mostly for singleton services,
+            where tokens are always meant for the same resource and the only
+            changes are in the scopes.
+            In AAD, tokens can be issued for multiple 3rd party resources.
+            You can ask authorization code for multiple resources,
+            but when you redeem it, the token is for only one intended
+            recipient, called audience.
+            So the developer need to specify a scope so that we can restrict the
+            token to be issued for the corresponding audience.
+        """
+        # If scope is absent on the wire, STS will give you a token associated
+        # to the FIRST scope sent during the authorization request.
+        # So in theory, you can omit scope here when you were working with only
+        # one scope. But, MSAL decorates your scope anyway, so they are never
+        # really empty.
+        return Client(
+            self.client_id, token_endpoint=self.authority.token_endpoint,
+            default_body=self._build_auth_parameters(
+                self.client_credential,
+                self.authority.token_endpoint, self.client_id)
+            ).obtain_token_with_authorization_code(
+                code, redirect_uri=redirect_uri,
+                data={"scope": decorate_scope(scope, self.client_id)},
+            )
 
     def acquire_token_silent(
             self, scope,
@@ -112,83 +215,6 @@ class ConfidentialClientApplication(ClientApplication):  # server-side web app
             ).obtain_token_with_client_credentials(
                 scope=scope,  # This grant flow requires no scope decoration
                 )
-
-    def get_authorization_request_url(
-            self,
-            scope,
-            # additional_scope=None,  # Not yet implemented
-            login_hint=None,
-            state=None,  # Recommended by OAuth2 for CSRF protection
-            policy='',
-            redirect_uri=None,
-            authority=None,  # By default, it will use self.authority;
-                             # Multi-tenant app can use new authority on demand
-            extra_query_params=None,  # None or a dictionary
-            ):
-        """Constructs a URL for you to start a Authorization Code Grant.
-
-        :param scope: Scope refers to the resource that will be used in the
-            resulting token's audience.
-        :param additional_scope: Additional scope is a concept only in AAD.
-            It refers to other resources you might want to prompt to consent
-            for in the same interaction, but for which you won't get back a
-            token for in this particular operation.
-            (Under the hood, we simply merge scope and additional_scope before
-            sending them on the wire.)
-        :param str state: Recommended by OAuth2 for CSRF protection.
-        """
-        the_authority = Authority(authority) if authority else self.authority
-        client = Client(
-            self.client_id,
-            authorization_endpoint=the_authority.authorization_endpoint)
-        return client.build_auth_request_uri(
-            response_type="code",  # Using Authorization Code grant
-            redirect_uri=redirect_uri, state=state, login_hint=login_hint,
-            scope=decorate_scope(scope, self.client_id, policy),
-            )
-
-    def acquire_token_with_authorization_code(
-            self,
-            code,
-            scope,  # Syntactically required. STS accepts empty value though.
-            redirect_uri=None,
-                # REQUIRED, if the "redirect_uri" parameter was included in the
-                # authorization request as described in Section 4.1.1, and their
-                # values MUST be identical.
-            policy=''
-            ):
-        """The second half of the Authorization Code Grant.
-
-        :param code: The authorization code returned from Authorization Server.
-        :param scope:
-
-            If you requested user consent for multiple resources, here you will
-            typically want to provide a subset of what you required in AC.
-
-            OAuth2 was designed mostly for singleton services,
-            where tokens are always meant for the same resource and the only
-            changes are in the scopes.
-            In AAD, tokens can be issued for multiple 3rd parth resources.
-            You can ask authorization code for multiple resources,
-            but when you redeem it, the token is for only one intended
-            recipient, called audience.
-            So the developer need to specify a scope so that we can restrict the
-            token to be issued for the corresponding audience.
-        """
-        # If scope is absent on the wire, STS will give you a token associated
-        # to the FIRST scope sent during the authorization request.
-        # So in theory, you can omit scope here when you were working with only
-        # one scope. But, MSAL decorates your scope anyway, so they are never
-        # really empty.
-        return Client(
-            self.client_id, token_endpoint=self.authority.token_endpoint,
-            default_body=self._build_auth_parameters(
-                self.client_credential,
-                self.authority.token_endpoint, self.client_id)
-            ).acquire_token_with_authorization_code(
-                code, redirect_uri=redirect_uri,
-                scope=decorate_scope(scope, self.client_id, policy),
-                query={'p': policy} if policy else None)
 
     def acquire_token_on_behalf_of(
             self, user_assertion, scope, authority=None, policy=''):
--- tests/test_application.py
+++ tests/test_application.py
@@ -5,8 +5,6 @@ import logging
 from msal.application import *
 from tests import unittest
 
-from authcode import AuthCodeReceiver
-
 
 THIS_FOLDER = os.path.dirname(__file__)
 CONFIG_FILE = os.path.join(THIS_FOLDER, 'config.json')
@@ -14,12 +12,6 @@ CONFIG = {}
 if os.path.exists(CONFIG_FILE):
     with open(CONFIG_FILE) as conf:
         CONFIG = json.load(conf)
-
-def acquire_token_with_authorization_code(app, redirect_port, scope):
-    # Note: This func signature does not and should not require client_secret
-    fresh_auth_code = AuthCodeReceiver.acquire(
-        app.get_authorization_request_url(scope), redirect_port)
-    return app.acquire_token_with_authorization_code(fresh_auth_code, scope)
 
 
 @unittest.skipUnless("client_id" in CONFIG, "client_id missing")
@@ -41,6 +33,7 @@ class TestConfidentialClientApplication(unittest.TestCase):
         result = app.acquire_token_for_client(self.scope)
         self.assertIn('access_token', result)
 
+
 @unittest.skipUnless("client_id" in CONFIG, "client_id missing")
 class TestPublicClientApplication(unittest.TestCase):
 
@@ -58,47 +51,33 @@ class TestPublicClientApplication(unittest.TestCase):
             self.assertIn('access_token', result)
 
 
-# Note: This test case requires human interaction to obtain authorization code
-@unittest.skipUnless(os.path.exists(CONFIG_FILE), "%s missing" % CONFIG_FILE)
-class ToDo_TestConfidentialClientApplication(unittest.TestCase):
-    scope = ["https://graph.microsoft.com/.default"]
-    scope2 = ["User.Read"]
-    scope_rt = ["offline_access"]
+@unittest.skipUnless("client_id" in CONFIG, "client_id missing")
+class TestClientApplication(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.config = json.load(open(CONFIG_FILE))
-        cls.app = ConfidentialClientApplication(
-            cls.config['CLIENT_ID'], cls.config['CLIENT_SECRET'])
-        cls.token = acquire_token_with_authorization_code(
-            # Prepare a token. It will be shared among multiple test cases.
-            cls.app, cls.config.get('REDIRECTION_PORT', 8000), cls.scope2)
-    def test_get_authorization_request_url(self):
-        app = ConfidentialClientApplication(self.config['CLIENT_ID'], "secret")
-        url = app.get_authorization_request_url(self.scope2)
-        print("Authorization URL: {}".format(url))
-        self.assertIn("response_type=code", url)  # A weak check, for now
-        # After user consent, your redirect endpoint will be hit like this:
-        # http://localhost:8000/?code=blahblah&other_param=foo
+        cls.app = ClientApplication(
+            CONFIG["client_id"], client_credential=CONFIG.get("client_secret"))
 
-    @classmethod
-    def beautify(cls, json_payload):
-        return json.dumps(json_payload, indent=2)
+    def assertLoosely(self, result):
+        if "error" in result:
+            # Some of these errors are configuration issues, not library issues
+            if result["error"] == "invalid_grant":
+                raise unittest.SkipTest(result.get("error_description"))
+            self.assertEqual(result["error"], "interaction_required")
+        else:
+            self.assertIn('access_token', result)
 
-    def test_acquire_token_with_authorization_code(self):
-        # Actually we already obtain a token during this TestCase initialization
-        self.assertEqual(self.token.get('error_description'), None)
-        logging.info("Authorization Code Grant: %s", self.beautify(self.token))
-
-    def test_acquire_token_silent(self):
-        if 'refresh_token' not in self.token:
-            raise unittest.SkipTest("refresh_token not available")
-        token = self.app.acquire_token_silent(
-            self.scope2, refresh_token=self.token['refresh_token'])
-        self.assertEqual(token.get('error_description', ""), "")
-        if 'refresh_token' in token:
-            logging.warn(
-                "Authorization Server also issues a new Refresh Token: %s",
-                self.beautify(token))
-            self.token = token  # https://tools.ietf.org/html/rfc6749#section-6
+    @unittest.skipUnless("scope" in CONFIG, "Missing scope")
+    def test_auth_code(self):
+        from oauth2cli.authcode import obtain_auth_code
+        port = CONFIG.get("listen_port", 44331)
+        redirect_uri = "http://localhost:%s" % port
+        auth_request_uri = self.app.get_authorization_request_url(
+            CONFIG["scope"], redirect_uri=redirect_uri)
+        ac = obtain_auth_code(port, auth_uri=auth_request_uri)
+        self.assertNotEqual(ac, None)
+        result = self.app.acquire_token_with_authorization_code(
+            ac, CONFIG["scope"], redirect_uri=redirect_uri)
+        self.assertLoosely(result)
 
