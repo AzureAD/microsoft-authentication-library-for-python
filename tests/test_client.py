2612ae535f337d120bbde7b393647fe5e038cf3d
Ray Luo <rayluo.mba@gmail.com>
2018-09-23 17:37:52 -0700

    Granular tests/test_client.py

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,36 +7,55 @@ from oauth2cli.authcode import obtain_auth_code
 from tests import unittest
 
 
-
 THIS_FOLDER = os.path.dirname(__file__)
-CONFIG_FILE = os.path.join(THIS_FOLDER, 'config.json')
+CONFIG_FILENAME = "config.json"
+
+def load_conf(filename):
+    try:
+        with open(filename) as f:
+            return json.load(f)
+    except:
+        logging.warn("Unable to find/read JSON configuration %s" % filename)
+
+CONFIG = load_conf(os.path.join(THIS_FOLDER, 'config.json')) or {}
 
 
+# Since the OAuth2 specs uses snake_case, this test config also uses snake_case
+@unittest.skipUnless("client_id" in CONFIG, "client_id missing")
 class TestClient(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        with open(CONFIG_FILE) as f:
-            cls.conf = json.load(f)
         cls.client = Client(
-            cls.conf['client_id'], cls.conf['client_secret'],
-            authorization_endpoint=cls.conf.get("authorization_endpoint"),
-            token_endpoint=cls.conf["token_endpoint"])
+            CONFIG['client_id'],
+            client_secret=CONFIG.get('client_secret'),
+            authorization_endpoint=CONFIG.get("authorization_endpoint"),
+            token_endpoint=CONFIG.get("token_endpoint"))
 
+    @unittest.skipUnless("client_secret" in CONFIG, "client_secret missing")
     def test_client_credentials(self):
         result = self.client.obtain_token_with_client_credentials(
-            self.conf['scope'])
+            CONFIG.get('scope'))
         self.assertIn('access_token', result)
 
+    @unittest.skipUnless(
+        "username" in CONFIG and "password" in CONFIG, "username/password missing")
     def test_username_password(self):
         result = self.client.obtain_token_with_username_password(
-            self.conf["username"], self.conf["password"],
-            data={"resource": self.conf.get("resource")},  # MSFT AAD V1 only
-            scope=self.conf.get("scope"))
-        self.assertIn('access_token', result)
+            CONFIG["username"], CONFIG["password"],
+            data={"resource": CONFIG.get("resource")},  # MSFT AAD V1 only
+            scope=CONFIG.get("scope"))
+        if "error" in result:
+            if result["error"] == "invalid_grant":
+                raise unittest.SkipTest(result.get("error_description"))
+            self.assertEqual(result["error"], "interaction_required")
+        else:
+            self.assertIn('access_token', result)
 
+    @unittest.skipUnless(
+        "authorization_endpoint" in CONFIG, "authorization_endpoint missing")
     def test_auth_code(self):
-        port = self.conf.get("listen_port", 44331)
+        port = CONFIG.get("listen_port", 44331)
         auth_request_uri = self.client.build_auth_request_uri(
             "code", redirect_uri="http://localhost:%s" % port)
         ac = obtain_auth_code(port, auth_uri=auth_request_uri)
