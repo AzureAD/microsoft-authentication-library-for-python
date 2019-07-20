be474a3f94d8428b51f0511b34e9825fa048291a
Ray Luo <rayluo@microsoft.com>
2019-07-19 18:34:03 -0700

    Fix regression on credential client grant

--- msal/token_cache.py
+++ msal/token_cache.py
@@ -121,15 +121,16 @@ class TokenCache(object):
         id_token_claims = (
             decode_id_token(id_token, client_id=event["client_id"])
             if id_token else {})
-        client_info = (
-            json.loads(base64decode(response["client_info"]))
-            if "client_info" in response
-            else {  # ADFS scenario
+        client_info = {}
+        if "client_info" in response:  # We asked for it, and AAD will provide it
+            client_info = json.loads(base64decode(response["client_info"]))
+        elif id_token_claims:  # This would be an end user on ADFS-direct scenario
+            client_info = {
                 "uid": id_token_claims.get("sub"),
                 "utid": realm,  # which, in ADFS scenario, would typically be "adfs"
                 }
-            )
-        home_account_id = "{uid}.{utid}".format(**client_info)
+        home_account_id = (  # It would remain None in client_credentials flow
+            "{uid}.{utid}".format(**client_info) if client_info else None)
         target = ' '.join(event.get("scope", []))  # Per schema, we don't sort it
 
         with self._lock:
