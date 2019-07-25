5e8d530373a2930c9090a30a736062c8b4266ea9
Ray Luo <rayluo@microsoft.com>
2019-07-25 10:29:06 -0700

    UX improvement based on recent Device Flow tests

--- sample/device_flow_sample.py
+++ sample/device_flow_sample.py
@@ -51,10 +51,18 @@ if accounts:
 
 if not result:
     logging.info("No suitable token exists in cache. Let's get a new one from AAD.")
+
     flow = app.initiate_device_flow(scopes=config["scope"])
+    if "user_code" not in flow:
+        raise ValueError(
+            "Fail to create device flow. Err: %s" % json.dumps(flow, indent=4))
+
     print(flow["message"])
+    sys.stdout.flush()  # Some terminal needs this to ensure the message is shown
+
     # Ideally you should wait here, in order to save some unnecessary polling
-    # input("Press Enter after you successfully login from another device...")
+    # input("Press Enter after signing in from another device to proceed, CTRL+C to abort.")
+
     result = app.acquire_token_by_device_flow(flow)  # By default it will block
         # You can follow this instruction to shorten the block time
         #    https://msal-python.readthedocs.io/en/latest/#msal.PublicClientApplication.acquire_token_by_device_flow
