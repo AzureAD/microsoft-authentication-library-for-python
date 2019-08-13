131d26d291fe9087313f90b528def8ef69e0ccfd
Ray Luo <rayluo@microsoft.com>
2019-08-12 18:17:53 -0700

    Refactor to use new helper names

--- tests/test_assertion.py
+++ tests/test_assertion.py
@@ -1,15 +1,15 @@
 import json
 
-from msal.oauth2cli import JwtSigner
-from msal.oauth2cli.oidc import base64decode
+from msal.oauth2cli import JwtAssertionCreator
+from msal.oauth2cli.oidc import decode_part
 
 from tests import unittest
 
 
 class AssertionTestCase(unittest.TestCase):
     def test_extra_claims(self):
-        assertion = JwtSigner(key=None, algorithm="none").sign_assertion(
+        assertion = JwtAssertionCreator(key=None, algorithm="none").sign_assertion(
             "audience", "issuer", additional_claims={"client_ip": "1.2.3.4"})
-        payload = json.loads(base64decode(assertion.split(b'.')[1].decode('utf-8')))
+        payload = json.loads(decode_part(assertion.split(b'.')[1].decode('utf-8')))
         self.assertEqual("1.2.3.4", payload.get("client_ip"))
 
