5d55e17236281cb450f5c1c9b2ade6b0ad92bc0c
Nathan Walsh <nwalsh@linkedin.com>
2020-01-29 17:24:35 -0800

    refactor oauth2 to extra request information

--- msal/oauth2cli/oauth2.py
+++ msal/oauth2cli/oauth2.py
@@ -16,6 +16,8 @@ import requests
 
 
 string_types = (str,) if sys.version_info[0] >= 3 else (basestring, )
+DAE = "device_authorization_endpoint"
+RT = "refresh_token"
 
 
 class BaseClient(object):
@@ -116,54 +118,11 @@ class BaseClient(object):
                         #   Mock(status_code=200, json=Mock(return_value={}))
             **kwargs  # Relay all extra parameters to underlying requests
             ):  # Returns the json object came from the OAUTH2 response
-        _data = {'client_id': self.client_id, 'grant_type': grant_type}
-
-        if self.default_body.get("client_assertion_type") and self.client_assertion:
-            # See https://tools.ietf.org/html/rfc7521#section-4.2
-            encoder = self.client_assertion_encoders.get(
-                    self.default_body["client_assertion_type"], lambda a: a)
-            _data["client_assertion"] = encoder(
-                self.client_assertion()  # Do lazy on-the-fly computation
-                if callable(self.client_assertion) else self.client_assertion)
-
-        _data.update(self.default_body)  # It may contain authen parameters
-        _data.update(data or {})  # So the content in data param prevails
-        # We don't have to clean up None values here, because requests lib will.
-
-        if _data.get('scope'):
-            _data['scope'] = self._stringify(_data['scope'])
-
-        # Quoted from https://tools.ietf.org/html/rfc6749#section-2.3.1
-        # Clients in possession of a client password MAY use the HTTP Basic
-        # authentication.
-        # Alternatively, (but NOT RECOMMENDED,)
-        # the authorization server MAY support including the
-        # client credentials in the request-body using the following
-        # parameters: client_id, client_secret.
-        auth = None
-        if self.client_secret and self.client_id:
-            auth = (self.client_id, self.client_secret)  # for HTTP Basic Auth
-
-        if "token_endpoint" not in self.configuration:
-            raise ValueError("token_endpoint not found in configuration")
-        _headers = {'Accept': 'application/json'}
-        _headers.update(headers or {})
-        resp = (post or self.session.post)(
-            self.configuration["token_endpoint"],
-            headers=_headers, params=params, data=_data, auth=auth,
-            timeout=timeout or self.timeout,
-            **kwargs)
-        if resp.status_code >= 500:
-            resp.raise_for_status()  # TODO: Will probably retry here
-        try:
-            # The spec (https://tools.ietf.org/html/rfc6749#section-5.2) says
-            # even an error response will be a valid json structure,
-            # so we simply return it here, without needing to invent an exception.
-            return resp.json()
-        except ValueError:
-            self.logger.exception(
-                    "Token response is not in json format: %s", resp.text)
-            raise
+        request_kwargs = self.get_obtain_token_request_info(grant_type=grant_type, data=data)
+        request_kwargs.update(dict(params=params, timeout=timeout or self.timeout))
+        request_kwargs.update(**kwargs)
+        resp = (post or self.session.post)(**request_kwargs)
+        return self.validate_obtain_token_response(resp)
 
     def obtain_token_by_refresh_token(self, refresh_token, scope=None, **kwargs):
         # type: (str, Union[str, list, set, tuple]) -> dict
@@ -184,6 +143,62 @@ class BaseClient(object):
             return ' '.join(sorted(sequence))  # normalizing it, ascendingly
         return sequence  # as-is
 
+    def requires_assertion(self):
+        return self.default_body.get("client_assertion_type") and self.client_assertion
+
+    def requires_auth(self):
+        return self.client_secret and self.client_id
+
+    def validate_obtain_token_response(self, resp):
+        if resp.status_code >= 500:
+            resp.raise_for_status()  # TODO: Will probably retry here
+        try:
+            # The spec (https://tools.ietf.org/html/rfc6749#section-5.2) says
+            # even an error response will be a valid json structure,
+            # so we simply return it here, without needing to invent an exception.
+            return resp.json()
+        except ValueError:
+            self.logger.exception(
+                "Token response is not in json format: %s", resp.text)
+            raise
+
+    def get_obtain_token_request_info(self, grant_type, data=None, headers=None):
+        validate_token_endpoint(self.configuration)
+        data = self.build_obtain_token_request_data(grant_type=grant_type, data=data)
+
+        # Quoted from https://tools.ietf.org/html/rfc6749#section-2.3.1
+        # Clients in possession of a client password MAY use the HTTP Basic
+        # authentication.
+        # Alternatively, (but NOT RECOMMENDED,)
+        # the authorization server MAY support including the
+        # client credentials in the request-body using the following
+        # parameters: client_id, client_secret.
+        auth = (self.client_id, self.client_secret) if self.requires_auth() else None  # for HTTP Basic Auth
+
+        _headers = {'Accept': 'application/json'}
+        _headers.update(headers or {})
+
+        return {"url": self.configuration["token_endpoint"], "data": data, "auth": auth, "headers": _headers}
+
+    def build_obtain_token_request_data(self, grant_type, data=None):
+        _data = {'client_id': self.client_id, 'grant_type': grant_type}
+
+        if self.requires_assertion():
+            # See https://tools.ietf.org/html/rfc7521#section-4.2
+            encoder = self.client_assertion_encoders.get(
+                self.default_body["client_assertion_type"], lambda a: a)
+            _data["client_assertion"] = encoder(
+                self.client_assertion()  # Do lazy on-the-fly computation
+                if callable(self.client_assertion) else self.client_assertion)
+
+        _data.update(self.default_body)  # It may contain authen parameters
+        _data.update(data or {})  # So the content in data param prevails
+        # We don't have to clean up None values here, because requests lib will.
+
+        if _data.get('scope'):
+            _data['scope'] = self._stringify(_data['scope'])
+
+        return _data
 
 class Client(BaseClient):  # We choose to implement all 4 grants in 1 class
     """This is the main API for oauth2 client.
@@ -199,6 +214,63 @@ class Client(BaseClient):  # We choose to implement all 4 grants in 1 class
     GRANT_TYPE_JWT = "urn:ietf:params:oauth:grant-type:jwt-bearer"  # RFC7523
     grant_assertion_encoders = {GRANT_TYPE_SAML2: BaseClient.encode_saml_assertion}
 
+    def get_initiate_device_flows_request_info(self, scope):
+        return dict(url=self.configuration[DAE], data={"client_id": self.client_id,
+                                                       "scope": self._stringify(scope or [])})
+
+    def build_obtain_token_by_device_flow_data(self, data, flow):
+        data.update({
+            "client_id": self.client_id,
+            self.DEVICE_FLOW["DEVICE_CODE"]: flow["device_code"],
+        })
+        return data
+
+    def build_obtain_token_by_authorization_code_data(self, data, code, redirect_uri, scope):
+        data.update(code=code, redirect_uri=redirect_uri)
+        if scope:
+            data["scope"] = scope
+        if not self.client_secret:
+            # client_id is required, if the client is not authenticating itself.
+            # See https://tools.ietf.org/html/rfc6749#section-4.1.3
+            data["client_id"] = self.client_id
+
+        return data
+
+    def build_obtain_token_by_username_password_data(self, data, username, password, scope):
+        data.update(username=username, password=password, scope=scope)
+        return data
+
+    def build_obtain_token_for_client_data(self, data, scope):
+        data.update(scope=scope)
+        return data
+
+    def build_obtain_token_for_assertion_data(self, data, scope, assertion, grant_type):
+        encoder = self.grant_assertion_encoders.get(grant_type, lambda a: a)
+        data.update(scope=scope, assertion=encoder(assertion))
+        return data
+
+    def handle_valid_token_response(self, resp, _data, grant_type, params):
+        _resp = resp.copy()
+        refresh_token = _data.get(RT)
+        if grant_type == RT and RT in _resp and isinstance(refresh_token, dict):
+            _resp.pop(RT)  # So we skip it in on_obtaining_tokens(); it will
+            # be handled in self.obtain_token_by_refresh_token()
+        if "scope" in _resp:
+            scope = _resp["scope"].split()  # It is conceptually a set,
+            # but we represent it as a list which can be persisted to JSON
+        else:
+            # Note: The scope will generally be absent in authorization grant,
+            #       but our obtain_token_by_authorization_code(...) encourages
+            #       app developer to still explicitly provide a scope here.
+            scope = _data.get("scope")
+        self.on_obtaining_tokens({
+            "client_id": self.client_id,
+            "scope": scope,
+            "token_endpoint": self.configuration["token_endpoint"],
+            "grant_type": grant_type,  # can be used to know an IdToken-less
+            # response is for an app or for a user
+            "response": _resp, "params": params, "data": _data,
+        })
 
     def initiate_device_flow(self, scope=None, timeout=None, **kwargs):
         # type: (list, **dict) -> dict
@@ -215,13 +287,13 @@ class Client(BaseClient):  # We choose to implement all 4 grants in 1 class
         And possibly here
         https://tools.ietf.org/html/draft-ietf-oauth-device-flow-12#section-3.3.1
         """
-        DAE = "device_authorization_endpoint"
-        if not self.configuration.get(DAE):
-            raise ValueError("You need to provide device authorization endpoint")
-        flow = self.session.post(self.configuration[DAE],
-            data={"client_id": self.client_id, "scope": self._stringify(scope or [])},
+        validate_device_authorization_endpoint(self.configuration)
+        request_kwargs = self.get_initiate_device_flows_request_info(scope=scope)
+        request_kwargs.update(kwargs)
+        flow = self.session.post(
             timeout=timeout or self.timeout,
-            **kwargs).json()
+            **request_kwargs
+        ).json()
         flow["interval"] = int(flow.get("interval", 5))  # Some IdP returns string
         flow["expires_in"] = int(flow.get("expires_in", 1800))
         flow["expires_at"] = time.time() + flow["expires_in"]  # We invent this
@@ -234,13 +306,8 @@ class Client(BaseClient):  # We choose to implement all 4 grants in 1 class
         skew = 1
         if flow.get("latest_attempt_at", 0) + flow.get("interval", 5) - skew > now:
             warnings.warn('Attempted too soon. Please do time.sleep(flow["interval"])')
-        data = kwargs.pop("data", {})
-        data.update({
-            "client_id": self.client_id,
-            self.DEVICE_FLOW["DEVICE_CODE"]: flow["device_code"],
-            })
-        result = self._obtain_token(
-            self.DEVICE_FLOW["GRANT_TYPE"], data=data, **kwargs)
+        data = self.build_obtain_token_by_device_flow_data(data=kwargs.pop("data", {}), flow=flow)
+        result = self._obtain_token(self.DEVICE_FLOW["GRANT_TYPE"], data=data, **kwargs)
         if result.get("error") == "slow_down":
             # Respecting https://tools.ietf.org/html/draft-ietf-oauth-device-flow-12#section-3.5
             flow["interval"] = flow.get("interval", 5) + 5
@@ -309,8 +376,7 @@ class Client(BaseClient):  # We choose to implement all 4 grants in 1 class
             maintain state between the request and callback.
         :param kwargs: Other parameters, typically defined in OpenID Connect.
         """
-        if "authorization_endpoint" not in self.configuration:
-            raise ValueError("authorization_endpoint not found in configuration")
+        validate_authorization_endpoint(self.configuration)
         authorization_endpoint = self.configuration["authorization_endpoint"]
         params = self._build_auth_request_params(
             response_type, redirect_uri=redirect_uri, scope=scope, state=state,
@@ -349,21 +415,15 @@ class Client(BaseClient):  # We choose to implement all 4 grants in 1 class
             We suggest to use the same scope already used in auth request uri,
             so that this library can link the obtained tokens with their scope.
         """
-        data = kwargs.pop("data", {})
-        data.update(code=code, redirect_uri=redirect_uri)
-        if scope:
-            data["scope"] = scope
-        if not self.client_secret:
-            # client_id is required, if the client is not authenticating itself.
-            # See https://tools.ietf.org/html/rfc6749#section-4.1.3
-            data["client_id"] = self.client_id
+        data = self.build_obtain_token_by_authorization_code_data(data=kwargs.pop("data", {}),
+                                                                  code=code, redirect_uri=redirect_uri, scope=scope)
         return self._obtain_token("authorization_code", data=data, **kwargs)
 
     def obtain_token_by_username_password(
             self, username, password, scope=None, **kwargs):
         """The Resource Owner Password Credentials Grant, used by legacy app."""
-        data = kwargs.pop("data", {})
-        data.update(username=username, password=password, scope=scope)
+        data = self.build_obtain_token_by_username_password_data(data=kwargs.pop("data", {}), username=username, password=password,
+                                                                 scope=scope)
         return self._obtain_token("password", data=data, **kwargs)
 
     def obtain_token_for_client(self, scope=None, **kwargs):
@@ -376,8 +436,7 @@ class Client(BaseClient):  # We choose to implement all 4 grants in 1 class
         or you can provide such extra parameters as `default_body` during the
         class initialization.
         """
-        data = kwargs.pop("data", {})
-        data.update(scope=scope)
+        data = self.build_obtain_token_for_client_data(scope=scope)
         return self._obtain_token("client_credentials", data=data, **kwargs)
 
     def __init__(self,
@@ -392,32 +451,11 @@ class Client(BaseClient):  # We choose to implement all 4 grants in 1 class
         self.on_updating_rt = on_updating_rt
 
     def _obtain_token(self, grant_type, params=None, data=None, *args, **kwargs):
-        RT = "refresh_token"
         _data = data.copy()  # to prevent side effect
-        refresh_token = _data.get(RT)
-        resp = super(Client, self)._obtain_token(
-            grant_type, params, _data, *args, **kwargs)
+        resp = super(Client, self)._obtain_token(grant_type, params, _data, *args, **kwargs)
         if "error" not in resp:
-            _resp = resp.copy()
-            if grant_type == RT and RT in _resp and isinstance(refresh_token, dict):
-                _resp.pop(RT)  # So we skip it in on_obtaining_tokens(); it will
-                               # be handled in self.obtain_token_by_refresh_token()
-            if "scope" in _resp:
-                scope = _resp["scope"].split()  # It is conceptually a set,
-                    # but we represent it as a list which can be persisted to JSON
-            else:
-                # Note: The scope will generally be absent in authorization grant,
-                #       but our obtain_token_by_authorization_code(...) encourages
-                #       app developer to still explicitly provide a scope here.
-                scope = _data.get("scope")
-            self.on_obtaining_tokens({
-                "client_id": self.client_id,
-                "scope": scope,
-                "token_endpoint": self.configuration["token_endpoint"],
-                "grant_type": grant_type,  # can be used to know an IdToken-less
-                                           # response is for an app or for a user
-                "response": _resp, "params": params, "data": _data,
-                })
+            self.handle_valid_token_response(resp=resp, _data=_data, grant_type=grant_type, params=params)
+
         return resp
 
     def obtain_token_by_refresh_token(self, token_item, scope=None,
@@ -465,8 +503,21 @@ class Client(BaseClient):  # We choose to implement all 4 grants in 1 class
             or :attr:`GRANT_TYPE_JWT`, the only two profiles defined in RFC 7521.
         :param scope: Optional. It must be a subset of previously granted scopes.
         """
-        encoder = self.grant_assertion_encoders.get(grant_type, lambda a: a)
-        data = kwargs.pop("data", {})
-        data.update(scope=scope, assertion=encoder(assertion))
+        data = self.build_obtain_token_for_assertion_data(data=kwargs.pop("data", {}), scope=scope,
+                                                          assertion=assertion, grant_type=grant_type)
         return self._obtain_token(grant_type, data=data, **kwargs)
 
+
+def validate_token_endpoint(config):
+    if "token_endpoint" not in config:
+        raise ValueError("token_endpoint not found in configuration")
+
+
+def validate_device_authorization_endpoint(config):
+    if not config.get(DAE):
+        raise ValueError("You need to provide device authorization endpoint")
+
+
+def validate_authorization_endpoint(config):
+    if "authorization_endpoint" not in config:
+        raise ValueError("authorization_endpoint not found in configuration")
