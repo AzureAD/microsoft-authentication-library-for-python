fcac05eda5355ba7cd6a21ab197172c3ba4a0bb4
Abhidnya Patil <abpati@microsoft.com>
2020-03-30 13:04:06 -0700

    cleaning up

--- msal/oauth2cli/__init__.py
+++ msal/oauth2cli/__init__.py
@@ -3,4 +3,3 @@ __version__ = "0.3.0"
 from .oidc import Client
 from .assertion import JwtAssertionCreator
 from .assertion import JwtSigner  # Obsolete. For backward compatibility.
-from .http import HttpClient
