36c60cada96b7b2df5dbabb1fcec956fdd2a70d6
Ray Luo <rayluo@microsoft.com>
2020-07-24 17:52:52 -0700

    Update application.py

--- msal/application.py
+++ msal/application.py
@@ -21,7 +21,7 @@ from .token_cache import TokenCache
 
 
 # The __init__.py will import this. Not the other way around.
-__version__ = "1.4.2"
+__version__ = "1.4.3"
 
 logger = logging.getLogger(__name__)
 
