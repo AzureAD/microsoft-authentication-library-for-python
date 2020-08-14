3209f155d017c5dc659de07849c9c88773b86aca
Abhidnya <patil.abhidnya@gmail.com>
2020-08-13 17:55:41 -0700

    small change in comment placing

--- msal/application.py
+++ msal/application.py
@@ -837,11 +837,11 @@ class PublicClientApplication(ClientApplication):  # browser app or mobile app
             flow,
             data=dict(
                 kwargs.pop("data", {}),
-                code=flow["device_code"],
-                claims=_merge_claims_and_capabilities(self._client_capabilities, claims)),
-                # 2018-10-4 Hack:
-                # during transition period,
-                # service seemingly need both device_code and code parameter.
+                code=flow["device_code"],  # 2018-10-4 Hack:
+                    # during transition period,
+                    # service seemingly need both device_code and code parameter.
+                claims=_merge_claims_and_capabilities(self._client_capabilities, claims),
+            ),
             headers={
                 CLIENT_REQUEST_ID:
                     flow.get(self.DEVICE_FLOW_CORRELATION_ID) or _get_new_correlation_id(),
