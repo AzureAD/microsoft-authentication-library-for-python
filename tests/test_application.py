e9a8c565ba5540890cfafb240bef806867561fbb
Abhidnya <patil.abhidnya@gmail.com>
2020-08-31 16:10:09 -0700

    Client capabilities (#240)

--- tests/test_application.py
+++ tests/test_application.py
@@ -2,6 +2,7 @@
 # so this test_application file contains only unit tests without dependency.
 from msal.application import *
 import msal
+from msal.application import _merge_claims_challenge_and_capabilities
 from tests import unittest
 from tests.test_token_cache import TokenCacheTestCase
 from tests.http_client import MinimalHttpClient, MinimalResponse
@@ -267,3 +268,45 @@ class TestClientApplicationForAuthorityMigration(unittest.TestCase):
         with self.assertRaises(ExpectedBehavior):
             self.app.acquire_token_silent(["different scope"], self.account)
 
+
+class TestApplicationForClientCapabilities(unittest.TestCase):
+
+    def test_capabilities_and_id_token_claims_merge(self):
+        client_capabilities = ["llt", "ssm"]
+        claims_challenge = '''{"id_token": {"auth_time": {"essential": true}}}'''
+        merged_claims = '''{"id_token": {"auth_time": {"essential": true}}, 
+                        "access_token": {"xms_cc": {"values": ["llt", "ssm"]}}}'''
+        # Comparing  dictionaries as JSON object order differs based on python version
+        self.assertEqual(
+            json.loads(merged_claims),
+            json.loads(_merge_claims_challenge_and_capabilities(
+                client_capabilities, claims_challenge)))
+
+    def test_capabilities_and_id_token_claims_and_access_token_claims_merge(self):
+        client_capabilities = ["llt", "ssm"]
+        claims_challenge = '''{"id_token": {"auth_time": {"essential": true}}, 
+                 "access_token": {"nbf":{"essential":true, "value":"1563308371"}}}'''
+        merged_claims = '''{"id_token": {"auth_time": {"essential": true}},
+                        "access_token": {"nbf": {"essential": true, "value": "1563308371"},
+                                        "xms_cc": {"values": ["llt", "ssm"]}}}'''
+        # Comparing  dictionaries as JSON object order differs based on python version
+        self.assertEqual(
+            json.loads(merged_claims),
+            json.loads(_merge_claims_challenge_and_capabilities(
+                client_capabilities, claims_challenge)))
+
+    def test_no_capabilities_only_claims_merge(self):
+        claims_challenge = '''{"id_token": {"auth_time": {"essential": true}}}'''
+        self.assertEqual(
+            json.loads(claims_challenge),
+            json.loads(_merge_claims_challenge_and_capabilities(None, claims_challenge)))
+
+    def test_only_client_capabilities_no_claims_merge(self):
+        client_capabilities = ["llt", "ssm"]
+        merged_claims = '''{"access_token": {"xms_cc": {"values": ["llt", "ssm"]}}}'''
+        self.assertEqual(
+            json.loads(merged_claims),
+            json.loads(_merge_claims_challenge_and_capabilities(client_capabilities, None)))
+
+    def test_both_claims_and_capabilities_none(self):
+        self.assertEqual(_merge_claims_challenge_and_capabilities(None, None), None)
