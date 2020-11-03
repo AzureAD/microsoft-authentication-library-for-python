03be495b09f023a3aefebf348abf42b9fc4168f4
Ray Luo <rayluo.mba@gmail.com>
2020-11-03 01:48:25 -0800

    Implement obtain_token_by_browser()

--- oauth2cli/oidc.py
+++ oauth2cli/oidc.py
@@ -193,3 +193,50 @@ class Client(oauth2.Client):
                     (nonce_in_id_token, expected_hash))
         return result
 
+    def obtain_token_by_browser(
+            self,
+            display=None,
+            prompt=None,
+            max_age=None,
+            ui_locales=None,
+            id_token_hint=None,  # It is relevant,
+                # because this library exposes raw ID token
+            login_hint=None,
+            acr_values=None,
+            **kwargs):
+        """A native app can use this method to obtain token via a local browser.
+
+        Internally, it implements nonce to mitigate replay attack.
+        It also implements PKCE to mitigate the auth code interception attack.
+
+        :param string display: Defined in
+            `OIDC <https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest>`_.
+        :param string prompt: Defined in
+            `OIDC <https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest>`_.
+        :param int max_age: Defined in
+            `OIDC <https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest>`_.
+        :param string ui_locales: Defined in
+            `OIDC <https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest>`_.
+        :param string id_token_hint: Defined in
+            `OIDC <https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest>`_.
+        :param string login_hint: Defined in
+            `OIDC <https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest>`_.
+        :param string acr_values: Defined in
+            `OIDC <https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest>`_.
+
+        See :func:`oauth2.Client.obtain_token_by_browser` in parent class
+        for descriptions on other parameters and return value.
+        """
+        filtered_params = {k:v for k, v in dict(
+            prompt=prompt,
+            display=display,
+            max_age=max_age,
+            ui_locales=ui_locales,
+            id_token_hint=id_token_hint,
+            login_hint=login_hint,
+            acr_values=acr_values,
+            ).items() if v is not None}  # Filter out None values
+        return super(Client, self).obtain_token_by_browser(
+            auth_params=dict(kwargs.pop("auth_params", {}), **filtered_params),
+            **kwargs)
+
