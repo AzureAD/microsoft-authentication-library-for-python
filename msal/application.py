896fbedd3adf347dea737a33f032e165ca343653
Ray Luo <rayluo@microsoft.com>
2021-03-08 10:01:44 -0800

    Bumping version number

--- msal/application.py
+++ msal/application.py
@@ -21,7 +21,7 @@ from .token_cache import TokenCache
 
 
 # The __init__.py will import this. Not the other way around.
-__version__ = "1.9.0"
+__version__ = "1.10.0"
 
 logger = logging.getLogger(__name__)
 
