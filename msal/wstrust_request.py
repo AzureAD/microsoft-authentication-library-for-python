034b9ae2f40dbf7872d97dc2eecac7eeda0181c0
Ray Luo <rayluo@microsoft.com>
2022-02-08 03:04:14 -0800

    Actionable exception from ADFS ROPC

--- msal/wstrust_request.py
+++ msal/wstrust_request.py
@@ -44,8 +44,8 @@ def send_request(
             soap_action = Mex.ACTION_2005
         elif '/trust/13/usernamemixed' in endpoint_address:
             soap_action = Mex.ACTION_13
-    assert soap_action in (Mex.ACTION_13, Mex.ACTION_2005), (  # A loose check here
-        "Unsupported soap action: %s" % soap_action)
+    if soap_action not in (Mex.ACTION_13, Mex.ACTION_2005):
+        raise ValueError("Unsupported soap action: %s" % soap_action)
     data = _build_rst(
         username, password, cloud_audience_urn, endpoint_address, soap_action)
     resp = http_client.post(endpoint_address, data=data, headers={
