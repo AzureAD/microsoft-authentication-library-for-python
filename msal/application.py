1e72b3ff7aae330d5464d4eec6def8723c4cf565
Bryant <bryanr@microsoft.com>
2022-08-02 15:20:48 -0700

    Update application.py

--- msal/application.py
+++ msal/application.py
@@ -1013,6 +1013,8 @@ class ClientApplication(object):
         return list(grouped_accounts.values())
 
     def _get_authority_aliases(self, instance):
+        if self.authority.is_adfs:
+            return []
         if not self.authority_groups:
             resp = self.http_client.get(
                 "https://login.microsoftonline.com/common/discovery/instance?api-version=1.1&authorization_endpoint=https://login.microsoftonline.com/common/oauth2/authorize",
