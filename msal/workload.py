1b5f85b0287190480ce6b68226887a6755db75b4
Ray Luo <rayluo@microsoft.com>
2023-04-07 11:21:18 -0700

    Just jogging down some findings

---
+++ msal/workload.py
@@ -0,0 +1,2 @@
+# TODO: Whether/how to support workload identity? https://github.com/Azure/azure-workload-identity
+# This is the implementation in Azure SDK: https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/identity/azure-identity/azure/identity/_credentials/workload_identity.py
