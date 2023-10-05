8bab1dd6fea2aa6beb922eeae6c5c183c685cb4e
Ray Luo <rayluo@microsoft.com>
2023-10-04 22:19:41 -0700

    Explain how to use global token cache and app

--- sample/username_password_sample.py
+++ sample/username_password_sample.py
@@ -22,6 +22,7 @@ You can then run this sample with a JSON configuration file:
 import sys  # For simplicity, we'll read config file from 1st CLI param sys.argv[1]
 import json
 import logging
+import time
 
 import requests
 import msal
@@ -33,41 +34,54 @@ import msal
 
 config = json.load(open(sys.argv[1]))
 
-# Create a preferably long-lived app instance which maintains a token cache.
-app = msal.ClientApplication(
+# If for whatever reason you plan to recreate same ClientApplication periodically,
+# you shall create one global token cache and reuse it by each ClientApplication
+global_token_cache = msal.TokenCache()  # The TokenCache() is in-memory.
+    # See more options in https://msal-python.readthedocs.io/en/latest/#tokencache
+
+# Create a preferably long-lived app instance, to avoid the overhead of app creation
+global_app = msal.PublicClientApplication(
     config["client_id"], authority=config["authority"],
     client_credential=config.get("client_secret"),
-    # token_cache=...  # Default cache is in memory only.
-                       # You can learn how to use SerializableTokenCache from
-                       # https://msal-python.readthedocs.io/en/latest/#msal.SerializableTokenCache
+    token_cache=global_token_cache,  # Let this app (re)use an existing token cache.
+        # If absent, ClientApplication will create its own empty token cache
     )
 
-# The pattern to acquire a token looks like this.
-result = None
 
-# Firstly, check the cache to see if this end user has signed in before
-accounts = app.get_accounts(username=config["username"])
-if accounts:
-    logging.info("Account(s) exists in cache, probably with token too. Let's try.")
-    result = app.acquire_token_silent(config["scope"], account=accounts[0])
+def acquire_and_use_token():
+    # The pattern to acquire a token looks like this.
+    result = None
 
-if not result:
-    logging.info("No suitable token exists in cache. Let's get a new one from AAD.")
-    # See this page for constraints of Username Password Flow.
-    # https://github.com/AzureAD/microsoft-authentication-library-for-python/wiki/Username-Password-Authentication
-    result = app.acquire_token_by_username_password(
-        config["username"], config["password"], scopes=config["scope"])
+    # Firstly, check the cache to see if this end user has signed in before
+    accounts = global_app.get_accounts(username=config["username"])
+    if accounts:
+        logging.info("Account(s) exists in cache, probably with token too. Let's try.")
+        result = global_app.acquire_token_silent(config["scope"], account=accounts[0])
 
-if "access_token" in result:
-    # Calling graph using the access token
-    graph_data = requests.get(  # Use token to call downstream service
-        config["endpoint"],
-        headers={'Authorization': 'Bearer ' + result['access_token']},).json()
-    print("Graph API call result: %s" % json.dumps(graph_data, indent=2))
-else:
-    print(result.get("error"))
-    print(result.get("error_description"))
-    print(result.get("correlation_id"))  # You may need this when reporting a bug
-    if 65001 in result.get("error_codes", []):  # Not mean to be coded programatically, but...
-        # AAD requires user consent for U/P flow
-        print("Visit this to consent:", app.get_authorization_request_url(config["scope"]))
+    if not result:
+        logging.info("No suitable token exists in cache. Let's get a new one from AAD.")
+        # See this page for constraints of Username Password Flow.
+        # https://github.com/AzureAD/microsoft-authentication-library-for-python/wiki/Username-Password-Authentication
+        result = global_app.acquire_token_by_username_password(
+            config["username"], config["password"], scopes=config["scope"])
+
+    if "access_token" in result:
+        # Calling graph using the access token
+        graph_data = requests.get(  # Use token to call downstream service
+            config["endpoint"],
+            headers={'Authorization': 'Bearer ' + result['access_token']},).json()
+        print("Graph API call result: %s" % json.dumps(graph_data, indent=2))
+    else:
+        print("Token acquisition failed")  # Examine result["error_description"] etc. to diagnose error
+        print(result)
+        if 65001 in result.get("error_codes", []):  # Not mean to be coded programatically, but...
+            raise RuntimeError(
+                "AAD requires user consent for U/P flow to succeed. "
+                "Run acquire_token_interactive() instead.")
+
+
+while True:  # Here we mimic a long-lived daemon
+    acquire_and_use_token()
+    print("Press Ctrl-C to stop.")
+    time.sleep(5)  # Let's say your app would run a workload every X minutes.
+
