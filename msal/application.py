2ac9057b33f8293c1a83522583fccb6dacc6d7d7
Ugonna Akali <akaliugonna@microsoft.com>
2025-09-30 09:32:12 -0700

    add comment

--- msal/application.py
+++ msal/application.py
@@ -1844,12 +1844,12 @@ The reserved list: {}""".format(list(scope_set), list(reserved_scope)))
         [Deprecated] This API is deprecated for public client flows and will be removed in a future release.
         Use a more secure flow instead. 
         Migration guide: https://aka.ms/msal-ropc-migration
-        
+
         """
         is_confidential_app = self.client_credential or isinstance(
             self, ConfidentialClientApplication)
         if not is_confidential_app:
-            warnings.warn("""This API has been deprecated, please use a more secure flow.
+            warnings.warn("""This API has been deprecated for public client flows, please use a more secure flow.
             See https://aka.ms/msal-ropc-migration for migration guidance""", DeprecationWarning)
         claims = _merge_claims_challenge_and_capabilities(
                 self._client_capabilities, claims_challenge)
