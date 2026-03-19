bfb65a0ab7770239a51f7a3bead5932ed677edc1
Ryan Auld <RyAuld@microsoft.com>
2026-03-19 15:31:02 -0700

    Fix sku.py syntax error: remove stray 'and' prefix on docstring

--- msal/sku.py
+++ msal/sku.py
@@ -1,4 +1,4 @@
-and"""This module is from where we recieve the client sku name and version.
+"""This module is from where we recieve the client sku name and version.
 """
 
 # The __init__.py will import this. Not the other way around.
